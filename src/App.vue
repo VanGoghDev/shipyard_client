eecd893ead5681d754c2962dd099867daf0c4406
Firsov Kirill <kifirsov@rttv.ru>
2023-06-04 22:33:26 +0300

    Axios test request added

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,16 @@
 </template>
 
 <script setup>
-  import { computed } from 'vue';
-  import store from '../store/index.js'
+  import { computed, onMounted } from 'vue';
+  import store from '../store/index.js';
+  import axiosClient from './axiosClient.js';
 
   const id = computed(() => store.state.id);
+
+  onMounted(async() => {
+    let response = await axiosClient.get("products/1")
+    console.log(response.data)
+  })
 
   function incrementid() {
     store.commit('increment')
