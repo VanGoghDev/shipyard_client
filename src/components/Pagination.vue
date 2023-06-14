faec144d58067de35fa6e97f98593692c62c4b3e
Firsov Kirill <kifirsov@rttv.ru>
2023-06-14 21:50:24 +0300

    added pagination empty template

---
+++ src/components/Pagination.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class=""></div>
+    {{ pageNumber }}
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+// https://vuejsexamples.com/a-vue-3-component-to-make-pagination-based-on-vuejs-paginate/
+const props = defineProps({
+    post: {
+        pageCount: true,
+        clickHandler: {
+            type: Function,
+            required: true
+        },
+        prevText: {
+            type: String
+        },
+        nextText: {
+            type: String
+        },
+        containerClass: {
+            type: String
+        },
+        pageRange: {
+            type: Number
+        },
+        breakViewText: {
+            type: String
+        },
+        initialPage: {
+            type: Number
+        }
+    }
+})
+
+const pageNumber = ref(0);
+
+</script>
