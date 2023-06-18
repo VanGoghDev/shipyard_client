0daabd3c789ea73ed5999d200461c056ff898d2f
“Cyril <kifirigor@gmail.com>
2023-06-18 12:19:49 +0300

    refactor

--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -2,7 +2,7 @@
     <header>
         <navbar></navbar>
     </header>
-    <main>
+    <main clas="content" style="margin: 0 10vw">
         <router-view></router-view>
     </main>
     <footer style="display: flex; margin: auto auto 0">
@@ -14,3 +14,5 @@
 import navbar from './Navbar.vue'
 import foot from './Footer.vue'
 </script>
+
+<style scoped></style>
