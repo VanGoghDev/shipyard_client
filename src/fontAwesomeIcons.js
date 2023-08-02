38bc781e2ddcd6354ff8d45cf974ed4bdcdd21d8
Cyril Firsov <kifirigor@gmail.com>
2023-08-03 01:25:38 +0300

    moved fa setup to separate file

---
+++ src/fontAwesomeIcons.js
@@ -0,0 +1,15 @@
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+library.add(faBars)
+
+export default FontAwesomeIcon
